50c17d62bc944543e143561412ab2e3b414ff211
emir <emir.denasevic@infobip.com>
2016-11-02 08:49:34 +0100

    first commit

---
+++ src/main/java/org/nsi/alpha/NsiAlphaApplication.java
@@ -0,0 +1,12 @@
+package org.nsi.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsiAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsiAlphaApplication.class, args);
+	}
+}
---
+++ src/test/java/org/nsi/alpha/NsiAlphaApplicationTests.java
@@ -0,0 +1,18 @@
+package org.nsi.alpha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NsiAlphaApplication.class)
+@WebAppConfiguration
+public class NsiAlphaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
