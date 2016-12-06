f8c00dd1e3785828ef67f27c082b3b58d59ab2ab
ekusundzija <elma.kusundzija@infobip.com>
2016-12-07 00:04:15 +0100

    Configured mapping of jsp pages

--- src/main/java/org/nsi/alpha/NsiAlphaApplication.java
+++ src/main/java/org/nsi/alpha/NsiAlphaApplication.java
@@ -1,27 +1,18 @@
 package org.nsi.alpha;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@RestController
 @SpringBootApplication
-public class NsiAlphaApplication {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(NsiAlphaApplication.class);
-
-	@RequestMapping("/home")
-	String home() {
-		return "home";
+public class NsiAlphaApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NsiAlphaApplication.class);
 	}
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(NsiAlphaApplication.class)
-				.run(args);
-		LOGGER.info("ULOGUJ NESTO PAMETNO TU MOLIM TE");
-		LOGGER.warn("Hvala najljepsa");
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(NsiAlphaApplication.class, args);
 	}
 }
